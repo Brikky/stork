447ae70858f8c7c624101d0bec1f6ecc612beb3c
brikky <brandon.kerr160@topper.wku.edu>
2016-11-08 16:07:47 -0800

    removes duplicate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include OrderItemsHelper
+  helper_method :current_order
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
